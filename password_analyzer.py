4693a7bd4869d82c940c0f22565f24353d68046c
PARTHSARTHI SEN <103943979+Parthgamedev@users.noreply.github.com>
2026-06-01 20:42:15 +0530

    Create password_analyzer.py

---
+++ password_analyzer.py
@@ -0,0 +1,29 @@
+import re
+
+def check_password(password):
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+
+    if re.search("[A-Z]", password):
+        score += 1
+
+    if re.search("[a-z]", password):
+        score += 1
+
+    if re.search("[0-9]", password):
+        score += 1
+
+    if re.search("[!@#$%^&*()_+]", password):
+        score += 1
+
+    if score <= 2:
+        return "Weak"
+    elif score <= 4:
+        return "Medium"
+    else:
+        return "Strong"
+
+password = input("Enter Password: ")
+print("Password Strength:", check_password(password))
